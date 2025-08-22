4a041235c2ca03932a95f17637126a90dc1e1ed9
Ryan Amorim dos Santos Silva <824146845@ulife.com.br>
2025-08-22 11:17:43 -0300

    conhecendo variaveis.javascript

---
+++ variaveis.js
@@ -0,0 +1,57 @@
+
+var nome= "Ryan"
+console.log(nome)
+const altura= 1.75
+let idade=19
+//altura 1.80
+idade=2000000
+console.log("Meu nome é "+nome+', minha altura é '+altura+' e minha idade é '+idade+' anos')
+
+//String
+
+let texto='String'
+
+
+//Number
+
+let n1=10
+let n2=20
+let n3=30
+
+
+//Bolenao
+let Boolenao1=true
+let Boolenao2=false
+
+//null 
+let nulo=null
+
+//undefined
+let indefinido=undefined
+
+//object
+
+let objeto={nome:"Ryan"}
+
+//array
+
+let lista=["uva","banana","pera","pessego","morango"]
+
+//Conversão de tipos de Dados
+
+//String->Numeros
+
+let stringParaNumero1=Number('10')
+let stringParaNumero2=parseInt('10.5')//converte para inteiro
+
+let stringParaNumero3=parseFloat('10.5')
+
+//Numero -> String 
+
+let str = String(100);
+let str2= (100).toString();
+
+//Boolean->Numero
+
+let booleanParaNumero=Number(true);//1
+let booleanParaNumero2=Number(false);//0
