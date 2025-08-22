0970b1004bea975d1e31cc89c7a8a46dd93c2b33
Ryan Amorim dos Santos Silva <824146845@ulife.com.br>
2025-08-22 11:49:20 -0300

    conhecendo funcoes.javascript

---
+++ funcoes.js
@@ -0,0 +1,26 @@
+//funções declarativas
+function saudacao(nome){
+    return `Olá, ${nome}!`
+}
+console.log(saudacao("Ryan"))
+
+
+//Função anonima
+
+const soma = function(a,b){
+
+}
+
+
+function simples(){
+let a= 5
+let b=12
+c = a+b
+}
+console.log(soma("O valor de C é: "+c))
+
+function somar (a,b){
+
+    return a+b
+}
+console.log(somar(5,10))
